ab285fde9634485f0270bb58dc83099158765fdc
TahmidU <tahmiduddin@hotmail.co.uk>
2020-05-13 21:20:13 +0100

    Updated input.

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -7,22 +7,22 @@
 #include <vector>
 #include <array>
 
-void tokenise(const std::string &args, const int &row, std::array<std::array<int,9>,9> &matrix) 
+void tokenise(const std::string& args, const int& row, std::array<std::array<int, 9>, 9>& matrix)
 {
 	std::string word = "";
-	int arr_position = 0; //Track array position.
-	
-	for (auto x : args) 
+	int arr_position = 0; //track array position.
+
+	for (auto x : args)
 	{
-		//If positions run out then drop the others.
-		if (arr_position > 8) 
+		//if positions run out then drop the others.
+		if (arr_position > 8)
 			break;
 
-		if (x == ' ') 
+		if (x == ' ')
 		{
 			word.clear();
 		}
-		else 
+		else
 		{
 			//if char thats not a space is detected, add it to the word.
 			word = word + x;
@@ -32,7 +32,7 @@ void tokenise(const std::string &args, const int &row, std::array<std::array<int
 	}
 }
 
-void input_args(std::string &args, std::array<std::array<int, 9>, 9> & matrix)
+void input_args(std::string& args, std::array<std::array<int, 9>, 9>& matrix)
 {
 	for (int i = 0; i < 9; ++i)
 	{
@@ -40,12 +40,12 @@ void input_args(std::string &args, std::array<std::array<int, 9>, 9> & matrix)
 		args.clear();
 		std::getline(std::cin, args);
 
-		//User inputs into matrix
+		//user inputs into matrix
 		tokenise(args, i, matrix);
 	}
 }
 
-void display_matrix(const std::array<std::array<int,9>,9> &matrix) 
+void display_matrix(const std::array<std::array<int, 9>, 9>& matrix)
 {
 	for (int i = 0; i < 9; ++i)
 	{
@@ -60,7 +60,7 @@ void display_matrix(const std::array<std::array<int,9>,9> &matrix)
 	}
 }
 
-bool find_next(const std::array<std::array<int, 9>, 9> &matrix, point& curr_point)
+bool find_next(const std::array<std::array<int, 9>, 9>& matrix, point& curr_point)
 {
 	for (int i = 0; i < 9; i++)
 	{
@@ -68,9 +68,8 @@ bool find_next(const std::array<std::array<int, 9>, 9> &matrix, point& curr_poin
 		{
 			if (matrix[i][j] == 0)
 			{
-
+				//[i][j], where i is row and j is col therefore x=j and y=i.
 				curr_point = point(j, i);
-
 				return true;
 			}
 		}
@@ -79,52 +78,52 @@ bool find_next(const std::array<std::array<int, 9>, 9> &matrix, point& curr_poin
 	return false;
 }
 
-bool validate(const int &num, const std::array<std::array<int, 9>, 9> & matrix, const point& curr_point)
+bool validate(const int& num, const std::array<std::array<int, 9>, 9>& matrix, const point& curr_point)
 {
-	for (int i = 0; i < 9; i++) 
+	//check rows
+	for (int i = 0; i < 9; i++)
 	{
 		if (matrix[i][curr_point.get_x()] == num && curr_point.get_y() != i)
 		{
-			//std::cout << "x:" << curr_point.get_x() << ", y:" << curr_point.get_y() << std::endl;
 			return false;
 		}
 	}
 
+
+	//check columns
 	for (int j = 0; j < 9; j++)
 	{
 		if (matrix[curr_point.get_y()][j] == num && curr_point.get_x() != j)
 		{
-			//std::cout << "x:" << curr_point.get_x() << ", y:" << curr_point.get_y() << std::endl;
 			return false;
 		}
 	}
 
-	int box_x = curr_point.get_x() / 3;
-	int box_y = curr_point.get_y() / 3;
 
-	for (int i = box_x*3; i < 3; i++)
+	//check in the single 3*3 boxes to check for any clashes.
+	int box_x = curr_point.get_x() - (curr_point.get_x() % 3);
+	int box_y = curr_point.get_y() - (curr_point.get_y() % 3);
+
+	for (int i = box_y; i < box_y + 3; i++)
 	{
-		for (int j = box_y*3; j < 3; j++)
+		for (int j = box_x; j < box_x + 3; j++)
 		{
-			//std::cout << "i:" << i << ", j:" << j << std::endl;
-			if (matrix[i][j] == num)
+			if (matrix[i][j] == num && i != curr_point.get_y() && j != curr_point.get_x())
 			{
-				//std::cout << "x:" << curr_point.get_x() << ", y:" << curr_point.get_y() << std::endl;
 				return false;
 			}
 		}
 	}
 
-	if (matrix[curr_point.get_y()][curr_point.get_x()] != 0)
-		return false;
-
+	//if it passes all validations then its good.
 	return true;
 }
 
-bool sudoku_sort(std::array<std::array<int, 9>, 9> &matrix)
+bool sudoku_solve(std::array<std::array<int, 9>, 9>& matrix)
 {
-	point curr_point(0,0);
+	point curr_point(0, 0);
 
+	//find next 0.
 	if (!find_next(matrix, curr_point))
 		return true;
 
@@ -133,15 +132,20 @@ bool sudoku_sort(std::array<std::array<int, 9>, 9> &matrix)
 
 		if (validate(i, matrix, curr_point))
 		{
+			//if its valid, apply to current position.
 			matrix[curr_point.get_y()][curr_point.get_x()] = i;
 
-			if (sudoku_sort(matrix))
+			//sort for next 0. if next is unsolvable then...
+			if (sudoku_solve(matrix))
 				return true;
 
+			//reset this one and try another number.
 			matrix[curr_point.get_y()][curr_point.get_x()] = 0;
 		}
 
 	}
+
+	//backtrack!
 	return false;
 }
 
@@ -149,44 +153,49 @@ bool sudoku_sort(std::array<std::array<int, 9>, 9> &matrix)
 
 int main()
 {
-    std::cout << "Enter the numbers in order with spaces in between (e.g. 1 5 0 3 0 0 0 0 0). Leave a 0 for empty spaces..." << std::endl;
+	//zero would be our unassigned symbol since it keeps things simplier.
+	std::cout << "Enter the numbers in order with spaces in between (e.g. 1 5 0 3 0 0 0 0 0). Leave a 0 for empty spaces..." << std::endl;
 
-	std::array<std::array<int, 9>, 9> matrix = 
+	//Use the stuff bellow to skip inputting into the console.
+
+	std::array<std::array<int, 9>, 9> matrix =
 	{
 		{
-			{3, 0, 6, 5, 0, 8, 4, 0, 0},
-			{5, 2, 0, 0, 0, 0, 0, 0, 0},
-			{0, 8, 7, 0, 0, 0, 0, 3, 1},
-			{0, 0, 3, 0, 1, 0, 0, 8, 0},
-			{9, 0, 0, 8, 6, 3, 0, 0, 5},
-			{0, 5, 0, 0, 9, 0, 6, 0, 0},
-			{1, 3, 0, 0, 0, 0, 2, 5, 0},
-			{0, 0, 0, 0, 0, 0, 0, 7, 4},
-			{0, 0, 5, 2, 0, 6, 3, 0, 0}
+			{0, 1, 3, 4, 2, 0, 0, 8, 6},
+			{2, 0, 4, 6, 0, 0, 0, 0, 0},
+			{0, 8, 7, 0, 1, 0, 3, 0, 0},
+			{0, 0, 0, 0, 3, 0, 6, 0, 0},
+			{0, 6, 2, 5, 0, 0, 0, 0, 3},
+			{5, 0, 0, 7, 6, 4, 0, 9, 1},
+			{7, 0, 0, 0, 4, 0, 8, 1, 0},
+			{0, 4, 0, 8, 0, 0, 0, 6, 0},
+			{0, 0, 1, 2, 5, 6, 0, 3, 7}
 		}
 	};
 
+	if (sudoku_solve(matrix))
+		display_matrix(matrix);
+
+	//Uncomment the code bellow and comment the code above to manually type into the console.
+
 	//std::array<std::array<int, 9>, 9> matrix;
 
-	//takes in user inputs
-	std::string args;
-	
-	do
-	{
-		/*input_args(args, matrix);*/
+	////takes in user inputs
+	//std::string args;
 
-		std::cout << "Is this how the board looks [y/n]? " << std::endl;
-		display_matrix(matrix);
-		std::getline(std::cin, args);
-	} while (args != "y");
+	//do
+	//{
+	//	input_args(args, matrix);
 
-	/*if (sudoku_sort(matrix, curr_point))
-		display_matrix(matrix);
-	else
-		std::cout << "No solution found." << std::endl;*/
+	//	std::cout << "Is this how the board looks [y/n]? " << std::endl;
+	//	display_matrix(matrix);
+	//	std::getline(std::cin, args);
+	//} while (args != "y");
 
-	sudoku_sort(matrix);
-	display_matrix(matrix);
+	//if (sudoku_solve(matrix))
+	//	display_matrix(matrix);
+	//else
+	//	std::cout << "No solution found. Probably mal input." << std::endl;
 
 	return 0;
 }
